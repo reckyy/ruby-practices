efd67a4242ecc5dc28d405a1812534123033ac4c
reckyy <srreki98@gmail.com>
2023-09-03 13:26:21 +0900

    グループ名

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -76,6 +76,7 @@ def get_file_stat(all_files, file_list)
     file_stat << convert_to_file_permission(fs)
     file_stat << fs.nlink
     file_stat << Etc.getpwuid(fs.uid).name
+    file_stat << Etc.getgrgid(fs.gid).name
     file_list << file_stat
   end
 end

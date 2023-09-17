ded203d424406e75b25680886bb1c1b4854a25e3
reckyy <srreki98@gmail.com>
2023-09-17 11:41:44 +0900

    wcコマンド動いた。 とりあえず、表示方法にはこだわらず、オプションがフルである状態なら動かすことに成功

---
+++ 05.wc/wc.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+def run
+	opts_hash = ARGV.getopts('clw')
+	options = opts_hash.keys.select { |k| opts_hash[k] }.sort
+	if $stdin.tty?
+		wc_list = []
+		ARGV.each do |file|
+			content = open(file, 'r')
+      wc_list << wc(options, content.read)
+			wc_list << file
+		end
+		puts wc_list.join(' ')
+	else
+		content = $stdin.read
+		wc_list = wc(options, content)
+		puts wc_list.join(' ')
+	end
+end
+
+def wc(opts, f)
+	[
+    opts.include?('l') ? f.lines.size : nil,
+    opts.include?('w') ? f.split.size : nil,
+    opts.include?('c') ? f.bytesize : nil,
+	]
+end
+
+run

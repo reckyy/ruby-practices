a3a898d4685e94fdc776aa0f1e464e5f09edb3ea
reckyy <srreki98@gmail.com>
2023-09-17 16:34:36 +0900

    normalのwcコマンドの表示合わせた

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'debug'
 require 'optparse'
 
 def run
@@ -10,10 +11,12 @@ def run
 		wc_list = []
 		ARGV.each do |file|
 			content = open(file, 'r')
-      wc_list << wc(options, content.read)
-			wc_list << file
+      wc_list << wc(options, content.read, file)
 		end
-		puts wc_list.join(' ')
+    wc_list.each { |wl| adjust_elements(wl) }
+		wc_list.each do |wl|
+      puts wl.join(' ')
+    end
 	else
 		wc_list = wc(options, $stdin.read)
 		wc_list.map { |wl| print wl.to_s.rjust(6 + wl.to_s.length) }
@@ -21,12 +24,22 @@ def run
 	end
 end
 
-def wc(opts, f)
+def adjust_elements(elm)
+  if elm.first.is_a?(String)
+    elm.map! { |element| element.ljust(width) }
+  else
+    elm.map! { |element| element.to_s.rjust(7) }
+  end
+end
+
+def wc(opts, f, name)
 	[
     opts.include?('l') ? f.lines.size : nil,
     opts.include?('w') ? f.split.size : nil,
     opts.include?('c') ? f.bytesize : nil,
+    name
 	]
 end
 
 run
+

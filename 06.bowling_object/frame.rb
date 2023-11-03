b9c2ce590d0f48c295fd831afba53b62b82386bc
reckyy <srreki98@gmail.com>
2023-11-03 14:35:18 +0900

    ⚡️ feat: Frameクラス作成

---
+++ 06.bowling_object/frame.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Frame
+
+  attr_reader :shot1 :shot2 :shot3
+
+  def initialize(shot1, shot2, shot3: 0)
+    @shot1 = shot1
+    @shot2 = shot2
+    @shot3 = shot3
+  end
+
+  def sum
+    @shot1 + @shot2 + @shot3
+  end
+
+  def strike?
+    @shot1 == 10
+  end
+
+  def spare?
+    sum == 10 && !strike?
+  end
+
+	def double_strike?(next_frame, next_to_frame)
+	  next_frame && next_to_frame && next_frame.@shot1 == 10
+	end
+
+	def calc_strike_point(next_frame, next_to_frame)
+	  if double_strike?(next_frame, next_to_frame)
+	    20 + (next_to_frame.@shot1)
+	  else
+	    10 + next_frame.@shot1 + next_frame.@shot2
+	  end
+	end
+end

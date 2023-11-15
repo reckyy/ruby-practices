6098086403e31591fb89be111e6ec3db41529c7b
reckyy <srreki98@gmail.com>
2023-11-15 22:23:48 +0900

    🐛 fix: 配列scoresをShotクラスのインスタンスの配列に変更

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'frame'
+require 'debug'
 
 class Game
   def initialize
-    scores = ARGV[0].split(',')
+    scores = ARGV[0].split(',').map { |s| Shot.new(s) }
     @frames = []
     @index_of_frame10 = 18
     calc_index_of_frame10(scores)
@@ -13,7 +14,7 @@ class Game
 
   def calc_index_of_frame10(scores)
     scores.each_with_index do |s, i|
-      @index_of_frame10 -= 1 if s == Shot::STRIKE_SYMBOL
+      @index_of_frame10 -= 1 if s.symbol == Shot::STRIKE_SYMBOL
       break if i == @index_of_frame10 - 1
     end
   end
@@ -26,20 +27,17 @@ class Game
         break
       end
 
-      shot = Shot.new(s)
-      create_frame_by_shot(shot, shots)
+      create_frame_by_shot(s, shots)
     end
   end
 
   def create_frame_by_shot(shot, shots)
-    if shot.symbol == Shot::STRIKE_SYMBOL
-      frame = Frame.new(Shot::MAX_PINS, 0)
-      add_frame(frame)
+    if shot.pins == Shot::MAX_PINS
+      add_frame(Frame.new(shot.pins, 0))
     else
       shots << shot.pins
       if shots.size == 2
-        frame = Frame.new(shots[0], shots[1])
-        add_frame(frame)
+        add_frame(Frame.new(shots[0], shots[1]))
         shots.clear
       end
     end
@@ -50,8 +48,8 @@ class Game
   end
 
   def deal_frame10(scores)
-    tenth_frame_scores = scores[@index_of_frame10..].map { |s| Shot.new(s).pins }
-    frame = Frame.new(tenth_frame_scores[0], tenth_frame_scores[1], shot3: tenth_frame_scores[2])
+    tenth_frame_scores = scores[@index_of_frame10..]
+    frame = Frame.new(tenth_frame_scores[0].pins, tenth_frame_scores[1].pins, shot3: tenth_frame_scores[2].pins)
     add_frame(frame)
   end
 
